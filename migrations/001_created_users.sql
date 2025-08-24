d3f13dbf3750db372ef5c11d08c92b010dc9e7fb
Марат <marat.tskh@gmail.com>
2025-08-24 23:42:21 +0300

    fix: made columns title&body in notes UNIQUE and column email in users too

--- migrations/001_created_users.sql
+++ migrations/001_created_users.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    email TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
     hashed_password TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT NOW() NOT NULL
 );
--- migrations/002_created_notes.sql
+++ migrations/002_created_notes.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS notes (
     id SERIAL PRIMARY KEY, 
-    user_id INT REFERENCES users(id),
-    title TEXT NOT NULL,
-    body TEXT,
+    user_id INT NOT NULL REFERENCES users(id),
+    title TEXT NOT NULL UNIQUE,
+    body TEXT NOT NULL UNIQUE,
     created_at TIMESTAMP DEFAULT NOW()
 );
 
