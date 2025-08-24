4906b805617f856d732acc1121214c73f8df8836
Марат <marat.tskh@gmail.com>
2025-08-24 16:10:08 +0300

    feat: add Note some  routes to the main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"notes_service/internal/service"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	// подключаемся к базе
 	db, err := PostgresConnection()
 	if err != nil {
-		log.Fatalf("Ошибка подключения к базе данных: %v", err)
+		log.Fatal(err)
 	}
-	defer db.Close()
-	fmt.Println("hello")
+
+	svc := service.NewService(db)
+
+	router := echo.New()
+
+	// создаем группу api
+	api := router.Group("api")
+
+	// Note routes
+	api.GET("/note/:id", svc.GetNoteById)
+	api.POST("/note", svc.CreateNote)
+	api.PUT("/note/:id", svc.UpdateNoteById)
+	api.DELETE("/note/:id", svc.DeleteNoteById)
+
+	// запускаем сервер, чтобы слушал 8000 порт
+	router.Logger.Fatal(router.Start(":8000"))
 }
