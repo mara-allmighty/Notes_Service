14b31e488e23543fabed1deef66081a56f389545
Марат <marat.tskh@gmail.com>
2025-08-25 00:33:20 +0300

    feat auth: create restricted & accessible routes, add routes for auth

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"notes_service/internal/service"
+	"notes_service/middlewares"
 
 	"github.com/labstack/echo/v4"
 )
@@ -18,14 +19,21 @@ func main() {
 
 	router := echo.New()
 
-	// создаем группу api
-	api := router.Group("api")
+	// Auth routes
+	router.GET("/login", svc.LogIn)
+	router.POST("/signup", svc.SignUp)
 
-	// Note routes
-	api.GET("/note/:id", svc.GetNoteById)
-	api.POST("/note", svc.CreateNote)
-	api.PUT("/note/:id", svc.UpdateNoteById)
-	api.DELETE("/note/:id", svc.DeleteNoteById)
+	// -- Accessible routes --
+	a := router.Group("/api")
+	a.GET("/note/:id", svc.GetNoteById)
+
+	// -- Restricted routes --
+	r := router.Group("/api")
+	r.Use(middlewares.AuthMiddleware()) // attach AuthMiddleware to routes
+
+	r.POST("/note", svc.CreateNote)
+	r.PUT("/note/:id", svc.UpdateNoteById)
+	r.DELETE("/note/:id", svc.DeleteNoteById)
 
 	// запускаем сервер, чтобы слушал 8000 порт
 	router.Logger.Fatal(router.Start(":8000"))
