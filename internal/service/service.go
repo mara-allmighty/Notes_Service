00b54d730ca37b68ff51f38ab24a747c7300b45d
Марат <marat.tskh@gmail.com>
2025-08-24 16:08:29 +0300

    feat: add Service type

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,21 @@
 package service
+
+import (
+	"database/sql"
+	"notes_service/internal/notes"
+)
+
+type Service struct {
+	db      *sql.DB
+	notesDB *notes.NotesDB
+}
+
+func NewService(db *sql.DB) *Service {
+	svc := &Service{db: db}
+	svc.initDBs(db)
+	return svc
+}
+
+func (s *Service) initDBs(db *sql.DB) {
+	s.notesDB = notes.NewNotesDB(db)
+}
