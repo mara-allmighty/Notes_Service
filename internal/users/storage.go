7ca9981415ba011dcfec33abffd397e6320dc37b
Марат <marat.tskh@gmail.com>
2025-08-25 21:02:18 +0300

    fix: nothing important

--- internal/users/storage.go
+++ internal/users/storage.go
@@ -1,7 +1,7 @@
 package users
 
 type User struct {
-	Id       int `json:"id"`
-	Email    string
-	Password string
+	Id       int    `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
