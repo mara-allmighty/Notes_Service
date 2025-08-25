4942e4650ba6de3e67f491f8e5fe3578f199534e
Марат <marat.tskh@gmail.com>
2025-08-25 12:57:29 +0300

    fix: go.sum update

--- middlewares/storage.go
+++ middlewares/storage.go
@@ -4,6 +4,6 @@ import "github.com/golang-jwt/jwt/v5"
 
 // jwtCustomClaims are custom claims extending default ones.
 type JwtCustomClaims struct {
-	Username string `json:"name"`
+	Email string `json:"email"`
 	jwt.RegisteredClaims
 }
