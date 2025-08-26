ad2bae95d627aaa9c2513cae074ea5b7eebbadfa
Марат <marat.tskh@gmail.com>
2025-08-26 19:33:44 +0300

    feat: add APIServer struct

---
+++ cmd/api_server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"notes_service/internal/service"
+	"notes_service/logs"
+	"notes_service/middlewares"
+
+	"github.com/labstack/echo/v4"
+)
+
+type APIServer struct {
+	addr string
+}
+
+func NewAPIServer(addr string) *APIServer {
+	return &APIServer{addr: addr}
+}
+
+func (s *APIServer) Run() {
+	logger := logs.NewLogger(false)
+
+	db, err := PostgresConnection()
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	svc := service.NewService(db, logger)
+
+	router := echo.New()
+
+	// Auth routes
+	router.GET("/login", svc.LogIn)
+	router.POST("/signup", svc.SignUp)
+
+	// -- Restricted routes --
+	api := router.Group("/api")
+	// attach auth middleware
+	api.Use(middlewares.AuthMiddleware())
+
+	api.GET("/note/:id", svc.GetNoteById)
+	api.GET("/notes", svc.GetNotes)
+	api.POST("/note", svc.CreateNote)
+	api.PUT("/note/:id", svc.UpdateNoteById)
+	api.DELETE("/note/:id", svc.DeleteNoteById)
+
+	router.Logger.Fatal(router.Start(s.addr))
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,41 +1,6 @@
 package main
 
-import (
-	"notes_service/internal/service"
-	"notes_service/logs"
-	"notes_service/middlewares"
-
-	"github.com/labstack/echo/v4"
-)
-
 func main() {
-	// создаем логгер
-	logger := logs.NewLogger(false)
-
-	// подключаемся к базе
-	db, err := PostgresConnection()
-	if err != nil {
-		logger.Fatal(err)
-	}
-
-	svc := service.NewService(db, logger)
-
-	router := echo.New()
-
-	// Auth routes
-	router.GET("/login", svc.LogIn)
-	router.POST("/signup", svc.SignUp)
-
-	// -- Restricted routes --
-	api := router.Group("/api")
-	api.Use(middlewares.AuthMiddleware()) // attach AuthMiddleware to routes
-
-	api.GET("/note/:id", svc.GetNoteById)
-	api.GET("/notes", svc.GetNotes)
-	api.POST("/note", svc.CreateNote)
-	api.PUT("/note/:id", svc.UpdateNoteById)
-	api.DELETE("/note/:id", svc.DeleteNoteById)
-
-	// http://localhost:8000
-	router.Logger.Fatal(router.Start(":8000"))
+	apiServer := NewAPIServer(":8000")
+	apiServer.Run()
 }
